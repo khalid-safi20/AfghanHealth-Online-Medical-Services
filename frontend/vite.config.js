65268445ec017667dce782360062362ae3898fbf
Khalid Areen <khalidareen8@gmail.com>
2025-08-17 17:36:02 +0500

    Change Vite build output folder to build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
-})
+  build: {
+    outDir: 'build', // this tells Vite to output to 'build' instead of 'dist'
+  },
+});
